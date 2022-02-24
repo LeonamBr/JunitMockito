70cb142e09cda5e21df3db0788f85b49b50200c3
LeonamBr <62444284+LeonamBr@users.noreply.github.com>
2022-02-23 23:57:36 -0300

    Teste de Excecões

--- src/main/java/braga/leonam/services/VehicleRentService.java
+++ src/main/java/braga/leonam/services/VehicleRentService.java
@@ -13,14 +13,14 @@ public class VehicleRentService {
 	public VehicleRent rent(Client client, Vehicle vehicle, Integer days) {
 		
 		if (vehicle == null)
-			throw new NullObjectException("Vehicles can't be null");
+			throw new NullObjectException("Vehicle can't be null");
 		if (client == null)
 			throw new NullObjectException("Client can't be null");
 			
 		VehicleRent rent = new VehicleRent(client, vehicle, Instant.now(), Instant.now().plusSeconds(days*86400));
 	
 		if (rent.getValue() < 0)
-			throw new IllegalDaysEntryException("The rent day must be befoore the return day");
+			throw new IllegalDaysEntryException("The rent day must be before the return day");
 		
 		return rent;
 	}
--- src/test/java/braga/leonam/services/VehicleRentServiceTest.java
+++ src/test/java/braga/leonam/services/VehicleRentServiceTest.java
@@ -11,6 +11,8 @@ import braga.leonam.entities.Client;
 import braga.leonam.entities.MotorCycle;
 import braga.leonam.entities.Vehicle;
 import braga.leonam.entities.VehicleRent;
+import braga.leonam.exceptions.IllegalDaysEntryException;
+import braga.leonam.exceptions.NullObjectException;
 
 public class VehicleRentServiceTest {
 
@@ -21,7 +23,7 @@ public class VehicleRentServiceTest {
 	private Client client;
 
 	private VehicleRent rent;
-	
+
 	private VehicleRentService service;
 
 	@Before
@@ -31,23 +33,62 @@ public class VehicleRentServiceTest {
 		client = new Client("Leonam Braga", 33, Instant.parse("1989-12-01T00:00:00Z"));
 		service = new VehicleRentService();
 	}
-	
+
 	@Test
 	public void carBuildRentService() {
-		
+
 		rent = service.rent(client, car, 5);
+
+		Assert.assertTrue(rent.isInstanceOfCar(rent.getVehicle()) && rent.getClient().equals(client)
+				&& rent.getValue() == 1250.00);
+
+	}
+
+	@Test
+	public void motorCycleBuildRentService() {
+
+		rent = service.rent(client, motorCycle, 5);
+
+		Assert.assertTrue(rent.isInstanceOfMotorCycle(rent.getVehicle()) && rent.getClient().equals(client)
+				&& rent.getValue() == 750.00);
+
+	}
+
+	@Test
+	public void nullClientException() {
 		
-		Assert.assertTrue(rent.isInstanceOfCar(rent.getVehicle()) && rent.getClient().equals(client) && rent.getValue() == 1250.00);
+		client = null;
 		
+		try {
+			service.rent(client, car, 5);
+			Assert.fail();
+		}catch (NullObjectException e) {
+			Assert.assertEquals("Exception test: ", "Client can't be null", e.getMessage());
+		}	
 	}
 	
 	@Test
-	public void motorCycleBuildRentService() {
+	public void nullVehicleException() {
 		
-		rent = service.rent(client, motorCycle, 5);
+		car = null;
 		
-		Assert.assertTrue(rent.isInstanceOfMotorCycle(rent.getVehicle()) && rent.getClient().equals(client) && rent.getValue() == 750.00);
-		
+		try {
+			service.rent(client, car, 5);
+			Assert.fail();
+		}catch (NullObjectException e) {
+			Assert.assertEquals("Exception test: ", "Vehicle can't be null", e.getMessage());
+		}	
+	}
+	
+	@Test
+	public void illegalDaysException() {
+				
+		try {
+			service.rent(client, car, -5);
+			Assert.fail();
+		}catch (IllegalDaysEntryException e) {
+			Assert.assertEquals("Exception test: ", "The rent day must be before the return day", e.getMessage());
+		}	
 	}
 	
 }
